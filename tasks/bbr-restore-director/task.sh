7f3fe84f09db505354b5a8266ced04a278d71e08
Kiran Anil <anil007kiran@gmail.com>
2018-04-02 20:51:29 +0530

    Update task.sh

--- tasks/bbr-restore-director/task.sh
+++ tasks/bbr-restore-director/task.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 . "$(dirname $0)"/../../scripts/export-director-metadata
+
+echo $OPSMAN_KEY  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/ssh_access.pem
+chmod 600 ~/ssh_access.pem
+ssh-agent > ~/agent
+eval $(ssh-agent -s)
+ssh-add ~/ssh_access.pem
+
 echo we can run any bash command here!!!!
 expect - <<EOF
-spawn ssh -i "${OPSMAN_KEY}" ${OPSMAN_USER_EC2}@${OPSMAN_IP}
+spawn ssh -i ~/ssh_access.pem ${OPSMAN_USER_EC2}@${OPSMAN_IP}
 expect {
 "*you sure you want to continue*" {send "yes"}
 }
