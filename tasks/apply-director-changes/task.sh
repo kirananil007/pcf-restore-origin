507d9410a35fa20275e4fe8c7715ff0a6900787e
Kiran Anil <anil007kiran@gmail.com>
2018-04-02 16:44:20 +0530

    Update task.sh

--- tasks/apply-director-changes/task.sh
+++ tasks/apply-director-changes/task.sh
@@ -4,8 +4,7 @@
 
 
 ## removing the bosh_state file from opsman
-ssh -tt -i "${OPSMAN_KEY}" -o "StrictHostKeyChecking no" "${OPSMAN_USER_EC2}"@"${OPSMAN_IP}" 
-sudo cat /var/tempest/workspaces/default/deployments/bosh-state.json
+ssh -i "${OPSMAN_KEY}" "${OPSMAN_USER_EC2}"@"${OPSMAN_IP}" -t "sudo cat /var/tempest/workspaces/default/deployments/bosh-state.json"
 
 echo $PWD
 #sudo rm -rf /var/tempest/workspaces/default/deployments/bosh-state.json
