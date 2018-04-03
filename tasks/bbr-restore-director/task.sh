9110fc8f9bd51b57814d01e57e94d8002d2d5e49
Kiran Anil <anil007kiran@gmail.com>
2018-04-03 18:51:38 +0530

    Update task.sh

--- tasks/bbr-restore-director/task.sh
+++ tasks/bbr-restore-director/task.sh
@@ -2,7 +2,6 @@
 . "$(dirname $0)"/../../scripts/export-director-metadata
 
 
-
 echo $OPSMAN_KEY  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/ssh_access.pem
 chmod 600 ~/ssh_access.pem
 ssh-agent > ~/agent
@@ -21,30 +20,14 @@ EOF
 pwd
 cd ../../../director-backup-bucket
 ls -al
-#echo we can run any bash command here!!!!
-#expect - <<EOF
-#spawn ssh -i ~/ssh_access.pem ${OPSMAN_USER_EC2}@${OPSMAN_IP}
-#expect {
-#"*you sure you want to continue*" {send "yes"}
-#}
-##expect "*you sure you want to continue*"
-##send "yes\r"
-#send "sudo cat /var/tempest/workspaces/default/deployments/bosh-state.json\n"
-#expect "*$hostnam*>"
-#send "sudo su -\n"
-#expect "*$hostnam*>"
-#send "cd /var/tempest/workspaces/default/\n"
-#expect "*$hostnam*>"
-#send "bosh2 alias-env sst-director -e 10.0.16.5 --ca-cert root_ca_certificate\n"
-#expect "*$hostnam*>"
-#send "bosh2 -e sst-director login\n"
-#expect "Email():"
-#send "$BOSH_USER\r"
-#expect "Password ():"
-#send "$BOSH_PASSWORD\r"
-#expect eof
-#send "bosh2 -e sst-director vms\n"
-#expect "*$hostnam*>"
-#send "echo BOSH director restoration is successful!!!\n"
-#expect "*$hostnam*>"
-#EOF
+
+## extract the s3 bucket contents
+cd ../../../director-backup-bucket
+cp -r director-*.tar ../binary/
+cd ../binary/
+tar -xvf director-*.tar
+
+## the restoration of bosh director
+./bbr director --private-key-path <(echo "${BBR_PRIVATE_KEY}") --username bbr --host "${BOSH_ADDRESS}" restore --artifact-path 10.0.*
+
+
