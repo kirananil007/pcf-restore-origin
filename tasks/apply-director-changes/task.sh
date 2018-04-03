b5c6bfc8dd001ee3f1e4f1c260770f84831a0cc6
Kiran Anil <anil007kiran@gmail.com>
2018-04-03 16:40:02 +0530

    Update task.sh

--- tasks/apply-director-changes/task.sh
+++ tasks/apply-director-changes/task.sh
@@ -3,14 +3,21 @@
 . "$(dirname $0)"/../../scripts/export-director-metadata
 
 
-## removing the bosh_state file from opsman
-ssh -i "${OPSMAN_KEY}" -o "StrictHostKeyChecking no" "${OPSMAN_USER_EC2}"@"${OPSMAN_IP}" -t "sudo cat /var/tempest/workspaces/default/deployments/bosh-state.json"
+### setting opsman keys
+echo $OPSMAN_KEY  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/ssh_access.pem
+chmod 600 ~/ssh_access.pem
+ssh-agent > ~/agent
+eval $(ssh-agent -s)
+ssh-add ~/ssh_access.pem
 
-echo $PWD
+###login to opsman and removing bosh state file
+ssh -i ~/ssh_access.pem -o "StrictHostKeyChecking no"  "${OPSMAN_USER_EC2}"@"${OPSMAN_IP}" <<EOF
 #sudo rm -rf /var/tempest/workspaces/default/deployments/bosh-state.json
-
+sudo ls -al /var/tempest/workspaces/default/deployments/bosh-state.json
+EOF
 
 ## applying changes to opsman director
+om_cmd
 #om_cmd apply-changes --ignore-warnings
 
 
