9cedf5a641351da2a9168319e342b60b8a60af0f
Kiran Anil <anil007kiran@gmail.com>
2018-04-03 17:41:31 +0530

    Update task.sh

--- tasks/bbr-restore-director/task.sh
+++ tasks/bbr-restore-director/task.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 . "$(dirname $0)"/../../scripts/export-director-metadata
 
-BOSH_CLIENT=${BOSH_CLIENT} BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET} bosh2 -e sst-director --ca-cert /var/tempest/workspaces/default/root_ca_certificate login
-BOSH_CLIENT=${BOSH_CLIENT} BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET} bosh2 -e sst-director --ca-cert /var/tempest/workspaces/default/root_ca_certificate vms
-echo "sucessful"
-#echo $OPSMAN_KEY  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/ssh_access.pem
-#chmod 600 ~/ssh_access.pem
-#ssh-agent > ~/agent
-#eval $(ssh-agent -s)
-#ssh-add ~/ssh_access.pem
 
+
+echo $OPSMAN_KEY  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/ssh_access.pem
+chmod 600 ~/ssh_access.pem
+ssh-agent > ~/agent
+eval $(ssh-agent -s)
+ssh-add ~/ssh_access.pem
+
+
+#login to opsman
+ssh -i ~/ssh_access.pem -o "StrictHostKeyChecking no"  "${OPSMAN_USER_EC2}"@"${OPSMAN_IP}" <<EOF
+cd /var/tempest/workspaces/default/
+sudo bosh2 alias-env sst-director -e ${BOSH_ADDRESS} --ca-cert root_ca_certificate
+BOSH_CLIENT=${BOSH_CLIENT} BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET} bosh2 -e sst-director --ca-cert /var/tempest/workspaces/default/root_ca_certificate login
+sudo bosh2 -e sst-director vms
+EOF
 #echo we can run any bash command here!!!!
 #expect - <<EOF
 #spawn ssh -i ~/ssh_access.pem ${OPSMAN_USER_EC2}@${OPSMAN_IP}
