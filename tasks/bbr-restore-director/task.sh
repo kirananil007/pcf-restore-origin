0f3a2b307ee69a2e22818177bfdf8425fd1836b3
Kiran Anil <anil007kiran@gmail.com>
2018-04-03 19:02:24 +0530

    Update task.sh

--- tasks/bbr-restore-director/task.sh
+++ tasks/bbr-restore-director/task.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 . "$(dirname $0)"/../../scripts/export-director-metadata
 
+## extract the s3 bucket contents
+cd ../../../director-backup-bucket
+cp -r director-*.tar ../binary/
+cd ../binary/
+tar -xvf director-*.tar
+
+## the restoration of bosh director
+./bbr director --private-key-path <(echo "${BBR_PRIVATE_KEY}") --username bbr --host "${BOSH_ADDRESS}" restore --artifact-path 10.0.*
+
 
 echo $OPSMAN_KEY  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/ssh_access.pem
 chmod 600 ~/ssh_access.pem
@@ -14,20 +23,11 @@ ssh -i ~/ssh_access.pem -o "StrictHostKeyChecking no"  "${OPSMAN_USER_EC2}"@"${O
 cd /var/tempest/workspaces/default/
 sudo bosh2 alias-env sst-director -e ${BOSH_ADDRESS} --ca-cert root_ca_certificate
 BOSH_CLIENT=${BOSH_CLIENT} BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET} bosh2 -e sst-director --ca-cert /var/tempest/workspaces/default/root_ca_certificate login
-sudo bosh2 -e sst-director vms
+sudo bosh2 -e "${BOSH_ADDRESS}" -d cf-965df3363954837f10b3 -n cck --resolution delete_disk_reference --resolution delete_vm_reference
 EOF
 
-pwd
-cd ../../../director-backup-bucket
-ls -al
 
-## extract the s3 bucket contents
-cd ../../../director-backup-bucket
-cp -r director-*.tar ../binary/
-cd ../binary/
-tar -xvf director-*.tar
 
-## the restoration of bosh director
-./bbr director --private-key-path <(echo "${BBR_PRIVATE_KEY}") --username bbr --host "${BOSH_ADDRESS}" restore --artifact-path 10.0.*
+
 
 
