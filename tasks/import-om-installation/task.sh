aae78454a5149981ba6b88ae728112c215b0a0ef
Kiran Anil <anil007kiran@gmail.com>
2018-04-04 18:13:26 +0530

    Update task.sh

--- tasks/import-om-installation/task.sh
+++ tasks/import-om-installation/task.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -eu
 
-#. "$(dirname $0)"/../../scripts/export-director-metadata
-
 ## extract the s3 bucket contents
 cd ../../../om-install
 ls -al
 
 ##import the installation file to opsman
-#om -k --target "${OPSMAN_URL}" --username "${OPSMAN_USERNAME}" --password "${OPSMAN_PASSWORD}" import-installation --installation installation.zip --decryption-passphrase ${PASSPHRASE}
+om -k --target "${OPSMAN_URL}" --username "${OPSMAN_USERNAME}" --password "${OPSMAN_PASSWORD}" import-installation --installation installation.zip --decryption-passphrase ${PASSPHRASE}
 
 echo "Import Complete!"
