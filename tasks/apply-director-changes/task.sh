e04e85912e9931c44972895a1f017ef685b5487a
Kiran Anil <anil007kiran@gmail.com>
2018-04-06 17:30:53 +0530

    Update task.sh

--- tasks/apply-director-changes/task.sh
+++ tasks/apply-director-changes/task.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eu
 
 ### setting opsman keys
-#echo $OPSMAN_KEY  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/ssh_access.pem
+echo $OPSMAN_KEY  | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/ssh_access.pem
 chmod 600 ~/ssh_access.pem
 ssh-agent > ~/agent
 eval $(ssh-agent -s)
 ssh-add ~/ssh_access.pem
 
 ###login to opsman and removing bosh state file
-ssh -i ~/ssh_access.pem -o "StrictHostKeyChecking no"  "${OPSMAN_USER_EC2}"@"${OPSMAN_IP}" <<EOF
+#ssh -i ~/ssh_access.pem -o "StrictHostKeyChecking no"  "${OPSMAN_USER_EC2}"@"${OPSMAN_IP}" <<EOF
 cd /var/tempest/workspaces/default/
 ls -al
 #sudo rm -rf /var/tempest/workspaces/default/deployments/bosh-state.json
