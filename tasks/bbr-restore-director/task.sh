b9283f427f501d8da1d2eeb2372068a16670bc9b
Kiran Anil <anil007kiran@gmail.com>
2018-06-08 16:55:21 +0530

    Update task.sh

--- tasks/bbr-restore-director/task.sh
+++ tasks/bbr-restore-director/task.sh
@@ -8,7 +8,7 @@ cd ../binary/
 tar -xvf director-*.tar
 
 ## the restoration of bosh director
-./bbr director --private-key-path <(echo "${BBR_PRIVATE_KEY}") --username bbr --host "${BOSH_ADDRESS}" restore --artifact-path 10.0.*
+./bbr director --private-key-path <(echo "${BBR_PRIVATE_KEY}") --username bbr --host "${BOSH_ADDRESS}" restore --artifact-path 192.168.*
 
 sleep 120
 
