18e0f90f89b4347902a5835979229bc1281099d5
jojinjg <jojinjg@gmail.com>
2018-06-12 18:16:13 +0000

    added the stemcell upload

--- tasks/apply-director-changes/task.sh
+++ tasks/apply-director-changes/task.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -eu
 
+##get stemcell contents to upload
+
+cd ../../../stemcell-upload
+om -k -t "${OPSMAN_URL}" --username "${OPSMAN_USERNAME}" --password "${OPSMAN_PASSWORD}" upload-stemcell -s light-bosh-stemcell-*
+
+sleep 20
+
+
+
 ### applying changes to opsman director
 om -k --target "${OPSMAN_URL}" --username "${OPSMAN_USERNAME}" --password "${OPSMAN_PASSWORD}" curl -p /api/v0/installations -x POST -d '{"deploy_products": "none","ignore_warnings": true}'
  
