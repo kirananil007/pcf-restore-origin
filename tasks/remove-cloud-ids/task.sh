a1560c8504d6a63af53ee8b7825fc110cdb460ef
Kiran Anil <anil007kiran@gmail.com>
2018-06-12 16:10:09 +0530

    comment added REMOVED THE CLOUD STALE IDS

--- tasks/remove-cloud-ids/task.sh
+++ tasks/remove-cloud-ids/task.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 BOSH_CLIENT=${BOSH_CLIENT} BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET} BOSH_CA_CERT=/var/tempest/workspaces/default/root_ca_certificate BOSH_ENVIRONMENT=${BOSH_ADDRESS} bosh2 -d cf-651c51d5ecec8e83e97b -n cck --resolution delete_disk_reference --resolution delete_vm_reference
+echo "REMOVED THE CLOUD STALE IDS"
